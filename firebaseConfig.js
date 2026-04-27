932678c06b4492a692451d5dafd115c2fee12b48
Zayah <zane0178@gmail.com>
2026-04-27 03:51:07 -0400

    new version pictures and suchforth

---
+++ firebaseConfig.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAiBTBPhgbBWXtDTMNw_bG4_anH-0abKYg",
+  authDomain: "obituary-undone-preview.firebaseapp.com",
+  projectId: "obituary-undone-preview",
+  storageBucket: "obituary-undone-preview.firebasestorage.app",
+  messagingSenderId: "313747331084",
+  appId: "1:313747331084:web:275ebadfefb64b04707629",
+  measurementId: "G-763YM3XPF1"
+};
+
+export default firebaseconfig;
